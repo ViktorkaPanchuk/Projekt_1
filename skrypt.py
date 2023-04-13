ea03ee28590a767ef93f3ad1022a2b820e43d38e
ViktorkaPanchuk <viktorkapanchuk@gmail.com>
2023-04-13 19:45:08 +0200

    Importowanie biblioteki argrarse. Napisanie kodu, który umożliwi podanie argumentów przez użytkowników dla transformacji XYZ_to_flh i flh_to_XYZ

--- skrypt.py
+++ skrypt.py
@@ -18,11 +18,12 @@ Created on Thu Apr 13 17:34:26 2023
 
 
 import numpy as np
+import argparse
 
-class Transformacje:
-    a = 6378137
-    e2 = 0.00669438002290  
-    def XYZ_to_flh(X,Y,Z,a,e2):
+class Transformacje: 
+    def XYZ_to_flh(self,X,Y,Z):
+        a = 6378137
+        e2 = 0.00669438002290 
         p = np.sqrt(X**2 + Y**2)
         f = np.arctan(Z/(p*(1-e2)))
         while True:
@@ -35,15 +36,41 @@ class Transformacje:
         l = np.arctan2(Y,X) 
         return(f,l,h)
     
-    def flh_to_XYZ(f,l,h,a,e2):
+    def flh_to_XYZ(self,f,l,h):
+        a = 6378137
+        e2 = 0.00669438002290 
         N = a/np.sqrt(1-e2*np.sin(f)**2)
         X = (N + h)*np.cos(f)*np.cos(l)
         Y = (N + h)*np.cos(f)*np.sin(l)
         Z = (N*(1-e2)+h)*np.sin(f)
         return(X,Y,Z)
     
-    
-    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Transformacje geodezyjne')
+    subparsers = parser.add_subparsers(title='Operacja', dest='operation', required=True)
+
+    parser_XYZ = subparsers.add_parser('XYZ', help='Transformuj współrzędne XYZ na flh')
+    parser_XYZ.add_argument('X', type=float, help='współrzędna X')
+    parser_XYZ.add_argument('Y', type=float, help='współrzędna Y')
+    parser_XYZ.add_argument('Z', type=float, help='współrzędna Z')
+
+    parser_flh = subparsers.add_parser('flh', help='Transformuj współrzędne flh na XYZ')
+    parser_flh.add_argument('f', type=float, help='współrzędna fi')
+    parser_flh.add_argument('l', type=float, help='współrzędna lambda')
+    parser_flh.add_argument('h', type=float, help='współrzędna h')
+
+    args = parser.parse_args()
+
+    transform = Transformacje()
+
+    if args.operation == 'XYZ':
+        result = transform.XYZ_to_flh(args.X, args.Y, args.Z)
+    elif args.operation == 'flh':
+        result = transform.flh_to_XYZ(args.f, args.l, args.h)
+
+    print(result)
+
+
     
     
     
