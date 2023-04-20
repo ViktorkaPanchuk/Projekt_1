af0ed8849b6c2ae379c70cd28fbf86c1b4c9b906
ViktorkaPanchuk <viktorkapanchuk@gmail.com>
2023-04-20 17:56:07 +0200

    Usunięcie powtarzających się definicji. Dodanie funkcji rad_to_dms, która przyjmuje wartość x i zwraca wynik w stopniach

--- skrypt.py
+++ skrypt.py
@@ -21,6 +21,17 @@ import numpy as np
 import argparse
 
 class Transformacje: 
+    def rad_to_dms(x):
+        sig = ''
+        if x<0:
+            sig = '-'
+            x = abs(x)
+        x = x*180/np.pi
+        d = int(x)
+        m = int (60*(x-d))
+        s = (x - d - m/60)*3600
+        print(sig, "%3d %2d %7.5f" %(d,abs(m),abs(s)))
+        
     def XYZ_to_flh(self,X,Y,Z):
         a = 6378137
         e2 = 0.00669438002290 
@@ -34,6 +45,8 @@ class Transformacje:
             if np.abs(fp - f) < (0.000001/206265):
                 break
         l = np.arctan2(Y,X) 
+        self.rad_to_dms(f)
+        self.rad_to_dms(l)
         return(f,l,h)
     
     def flh_to_XYZ(self,f,l,h):
@@ -49,11 +62,11 @@ class Transformacje:
     # z fi lam GRS80 do 2000
     
     def dms2rad(self, d, m, s):
-        kat_rad = radians(d + m/60 + s/3600)
+        kat_rad = np.radians(d + m/60 + s/3600)
         return kat_rad   
     
     def lambda0_2000(self, l_deg):
-        l = l_deg*pi/180
+        l = l_deg*np.pi/180
         if l<self.dms2rad(15, 00, 00):
             l0 = self.dms2rad(15, 00, 00)
             nr_strefy = 5
@@ -78,8 +91,8 @@ class Transformacje:
     
     def fl_80_2_gk2000(self, f, l): #f,l w stopniach 
         l0,nr_strefy = self.lambda0_2000(l)
-        f = f/180*pi
-        l = l/180*pi
+        f = f/180*np.pi
+        l = l/180*np.pi
          
         # elipsoida GRS80
         a = 6378137 #m
@@ -98,29 +111,20 @@ class Transformacje:
         sigma = a * (A0 * f - A2 * np.sin(2 * f) + A4 * np.sin(4 * f) - A6 * np.sin(6 * f))
         X_gk_2000 = sigma + ((d_l**2) / 2) * N * np.sin(f) * np.cos(f) * (1 + ((d_l**2) / 12) * ((np.cos(f))**2) * (5 - (t**2) + 9 * eta2 + 4 * eta2**2) + ((d_l**4) / 360) * ((np.cos(f))**4) * (61 - 58 * (t**2) + (t**4) + 270 * eta2 - 330 * eta2 * (t**2)))
         Y_gk_2000 = d_l * N * np.cos(f) * (1 + ((d_l**2) / 6) * ((np.cos(f))**2) * (1 - (t**2) + eta2) + ((d_l**4) / 120) * ((np.cos(f))**4) * (5 - 18 * (t**2) + (t**4) + 14 * eta2 - 58 * eta2 * (t**2)))
-        
         return(X_gk_2000, Y_gk_2000, nr_strefy)   
     
-    def GK_2_PL2000(self,X_gk_2000,Y_gk_2000,nr_strefy):
+    def GK_2_PL2000(self,X_gk,Y_gk,nr_strefy):
         m0 = 0.999923
         X2000 = X_gk * m0
         Y2000= m0 * Y_gk + nr_strefy * 1000000 + 500000
         return(X2000,Y2000)
     
-
-
     # z fi lam GRS80 do 1992
-    
-    def dms2rad(self, d, m, s):
-        kat_rad = radians(d + m/60 + s/3600)
-        return kat_rad   
-    
-    
+        
     def fl_80_2_gk1992(self, f, l): #f,l w stopniach 
         l0 = self.dms2rad(19, 00, 00)
-        
-        f = f/180*pi
-        l = l/180*pi
+        f = f/180*np.pi
+        l = l/180*np.pi
         
         # elipsoida GRS80
         a = 6378137 #m
@@ -139,54 +143,16 @@ class Transformacje:
         sigma = a * (A0 * f - A2 * np.sin(2 * f) + A4 * np.sin(4 * f) - A6 * np.sin(6 * f))
         X_gk_92 = sigma + ((d_l**2) / 2) * N * np.sin(f) * np.cos(f) * (1 + ((d_l**2) / 12) * ((np.cos(f))**2) * (5 - (t**2) + 9 * eta2 + 4 * eta2**2) + ((d_l**4) / 360) * ((np.cos(f))**4) * (61 - 58 * (t**2) + (t**4) + 270 * eta2 - 330 * eta2 * (t**2)))
         Y_gk_92 = d_l * N * np.cos(f) * (1 + ((d_l**2) / 6) * ((np.cos(f))**2) * (1 - (t**2) + eta2) + ((d_l**4) / 120) * ((np.cos(f))**4) * (5 - 18 * (t**2) + (t**4) + 14 * eta2 - 58 * eta2 * (t**2)))
-        
         return(X_gk_92, Y_gk_92)   
     
-    def GK_2_PL1992(X_gk_92,Y_gk_92):
+    def GK_2_PL1992(X_gk,Y_gk):
         m0 = 0.9993
         X1992 = X_gk * m0 - 5300000
         Y1992 = m0 * Y_gk + 500000
         return(X1992,Y1992)
     
     
-    
-    
-    
-    
-    
-    
-    
-    # z fi lam WGS84 do 2000
-    
-    
-    def dms2rad(self, d, m, s):
-        kat_rad = radians(d + m/60 + s/3600)
-        return kat_rad   
-    
-    def lambda0_2000(self, l_deg):
-        l = l_deg*pi/180
-        if l<self.dms2rad(15, 00, 00):
-            l0 = self.dms2rad(15, 00, 00)
-            nr_strefy = 5
-            print('l0 =', l0,'nr strefy = ', nr_strefy)
-        elif l<self.dms2rad(16, 30, 00):
-            l0 = self.dms2rad(15, 00, 00)
-            nr_strefy = 5
-            print('l0 =', l0,'nr strefy = ', nr_strefy)
-        elif l<self.dms2rad(19, 30, 00):
-            l0 = self.dms2rad(18, 00, 00)
-            nr_strefy = 6
-            print('l0 =', l0,'nr strefy = ', nr_strefy)
-        elif l<self.dms2rad(22, 30, 00):
-            l0 = self.dms2rad(21, 00, 00)
-            nr_strefy = 7
-            print('l0 =', l0,'nr strefy = ', nr_strefy)
-        else:
-            l0 = self.dms2rad(24, 00, 00)
-            nr_strefy = 8
-            print('l0 =', l0,'nr strefy = ', nr_strefy)
-        return l0, nr_strefy
-    
+   # z fi lam WGS84 do 2000
    # wgs
    #a = 6378137 # m	
    # e2 = 0.00335281068118231893543414612613
@@ -194,8 +160,8 @@ class Transformacje:
     
     def fl_84_2_gk2000(self, f, l): #f,l w stopniach 
         l0,nr_strefy = self.lambda0_2000(l)
-        f = f/180*pi
-        l = l/180*pi
+        f = f/180*np.pi
+        l = l/180*np.pi
          
         # elipsoida wgs84
         a = 6378137 # m	
@@ -214,32 +180,14 @@ class Transformacje:
         sigma = a * (A0 * f - A2 * np.sin(2 * f) + A4 * np.sin(4 * f) - A6 * np.sin(6 * f))
         X84_gk_2000 = sigma + ((d_l**2) / 2) * N * np.sin(f) * np.cos(f) * (1 + ((d_l**2) / 12) * ((np.cos(f))**2) * (5 - (t**2) + 9 * eta2 + 4 * eta2**2) + ((d_l**4) / 360) * ((np.cos(f))**4) * (61 - 58 * (t**2) + (t**4) + 270 * eta2 - 330 * eta2 * (t**2)))
         Y84_gk_2000 = d_l * N * np.cos(f) * (1 + ((d_l**2) / 6) * ((np.cos(f))**2) * (1 - (t**2) + eta2) + ((d_l**4) / 120) * ((np.cos(f))**4) * (5 - 18 * (t**2) + (t**4) + 14 * eta2 - 58 * eta2 * (t**2)))
-        
         return(X84_gk_2000, Y84_gk_2000, nr_strefy)   
-    
-    def GK_2_PL2000(self,X84_gk_2000,Y84_gk_2000,nr_strefy):
-        m0 = 0.999923
-        X2000 = X_gk * m0
-        Y2000= m0 * Y_gk + nr_strefy * 1000000 + 500000
-        return(X2000,Y2000)
-    
-    
-    
-    
-    
     
     # z fi lam wgs84 do 1992
     
-    def dms2rad(self, d, m, s):
-        kat_rad = radians(d + m/60 + s/3600)
-        return kat_rad   
-    
-    
-    def fl_80_2_gk1992(self, f, l): #f,l w stopniach 
+    def fl_84_2_gk1992(self, f, l): #f,l w stopniach 
         l0 = self.dms2rad(19, 00, 00)
-        
-        f = f/180*pi
-        l = l/180*pi
+        f = f/180*np.pi
+        l = l/180*np.pi
         
         # elipsoida wgs84
         a = 6378137 # m	
@@ -258,14 +206,9 @@ class Transformacje:
         sigma = a * (A0 * f - A2 * np.sin(2 * f) + A4 * np.sin(4 * f) - A6 * np.sin(6 * f))
         X84_gk_92 = sigma + ((d_l**2) / 2) * N * np.sin(f) * np.cos(f) * (1 + ((d_l**2) / 12) * ((np.cos(f))**2) * (5 - (t**2) + 9 * eta2 + 4 * eta2**2) + ((d_l**4) / 360) * ((np.cos(f))**4) * (61 - 58 * (t**2) + (t**4) + 270 * eta2 - 330 * eta2 * (t**2)))
         Y84_gk_92 = d_l * N * np.cos(f) * (1 + ((d_l**2) / 6) * ((np.cos(f))**2) * (1 - (t**2) + eta2) + ((d_l**4) / 120) * ((np.cos(f))**4) * (5 - 18 * (t**2) + (t**4) + 14 * eta2 - 58 * eta2 * (t**2)))
-        
         return(X84_gk_92, Y84_gk_92)   
     
-    def GK_2_PL1992(X84_gk_92,Y84_gk_92):
-        m0 = 0.9993
-        X1992 = X_gk * m0 - 5300000
-        Y1992 = m0 * Y_gk + 500000
-        return(X1992,Y1992)
+
 
 
 
